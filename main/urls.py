99e5f409f1c51243d6030822cebf2d8bbdb1b95b
Jonathan Kim <jkimbo@gmail.com>
2021-08-22 14:51:57 +0200

    Add graphene v3 implementation

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 from django.urls import path
 
-from strawberry.django.views import GraphQLView
+from strawberry.django.views import GraphQLView as StrawberryView
+from graphene_django.views import GraphQLView as GrapheneView
 
 from strawberry_api.schema import schema as strawberry_schema
+from graphene_api.schema import schema as graphene_schema
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("strawberry-graphql/", GraphQLView.as_view(schema=strawberry_schema)),
+    path("strawberry-graphql/", StrawberryView.as_view(schema=strawberry_schema)),
+    path(
+        "graphene-graphql", GrapheneView.as_view(schema=graphene_schema, graphiql=True)
+    ),
 ]
