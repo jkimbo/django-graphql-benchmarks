0cd9350b1194de564593c00c201d564b5b0e5d90
Jonathan Kim <jkimbo@gmail.com>
2021-08-22 17:26:24 +0200

    Add drf api

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from strawberry.django.views import GraphQLView as StrawberryView
 from graphene_django.views import GraphQLView as GrapheneView
+from rest_framework import routers
 
 from strawberry_api.schema import schema as strawberry_schema
 from graphene_api.schema import schema as graphene_schema
+from drf_api.serializers import MovieViewSet
+
+
+router = routers.DefaultRouter()
+router.register(r"movies", MovieViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -13,4 +19,5 @@ urlpatterns = [
     path(
         "graphene-graphql", GrapheneView.as_view(schema=graphene_schema, graphiql=True)
     ),
+    path("drf-api/", include(router.urls)),
 ]
