9c6938a2d9e4e0fd9ac996d711d1fd0ef928c5fb
Jonathan Kim <jkimbo@gmail.com>
2021-08-22 17:55:17 +0200

    Set debug to false

--- main/settings.py
+++ main/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from distutils.util import strtobool
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-h$$^224(@wre@*+^_+1-yqnergs%52-6vzeeywmgcq2gm-c0hd"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = strtobool(os.environ.get("DEBUG", None))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["django-graphql-benchmark.herokuapp.com"]
 
 
 # Application definition
