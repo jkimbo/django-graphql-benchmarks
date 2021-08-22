ce80860a1d59e4241be68b376252f83d3a96ff28
Jonathan Kim <jkimbo@gmail.com>
2021-08-22 18:19:37 +0200

    Make graphene crsf exempt

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.decorators.csrf import csrf_exempt
 
 from strawberry.django.views import GraphQLView as StrawberryView
 from graphene_django.views import GraphQLView as GrapheneView
@@ -18,7 +19,8 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("strawberry-graphql/", StrawberryView.as_view(schema=strawberry_schema)),
     path(
-        "graphene-graphql", GrapheneView.as_view(schema=graphene_schema, graphiql=True)
+        "graphene-graphql",
+        csrf_exempt(GrapheneView.as_view(schema=graphene_schema, graphiql=True)),
     ),
     path("drf-api/", include(router.urls)),
     path("json-api/top-250", top_250),
